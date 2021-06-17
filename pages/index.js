3775978847c3be01e462b2746b84f09c5ddfff64
endo@sria.co.jp <endo@sria.co.jp>
2021-06-17 09:22:30 +0900

    Linkの途中

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -13,11 +14,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        Read <Link href="/posts/first-post"><a>this page!</a></Link>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Learn{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
