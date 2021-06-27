6063ba51b6a4a18a1212378201b1d6fe71885934
endo@sria.co.jp <endo@sria.co.jp>
2021-06-27 20:13:24 +0900

    Typescript講座終了し、チュートリアル完了

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,7 +7,10 @@ import Image from 'next/image'
 const name = 'ENDO YUTA'
 export const siteTitle = 'Next.js Sample Website'
 
-export default function Layout({ children, home }) {
+export default function Layout({ children, home }: {
+  children: React.ReactNode
+  home?: boolean
+}) {
   return (
     <div className={styles.container}>
       <Head>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,9 @@ import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
+import { GetStaticProps } from 'next'
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData()
   return {
     props: {
